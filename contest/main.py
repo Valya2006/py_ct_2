ce0291f0f8cd3c8afb7cb5fab8bb87241ca43a05
valya <valia2006@DESKTOP-KJRGD7H.localdomain>
2026-04-03 18:02:34 +0300

    4

--- contest/main.py
+++ contest/main.py
@@ -27,6 +27,27 @@ def remove_duplicate_ids(obj: dict) -> dict:
     return result
 
 # В данной функции определите самостоятельно, что она принимает, а что возвращает
-def lazy():
+def lazy(n):
     """Решение для Задачи 4"""
-    pass
+    def decorator(func):
+        count = 0
+        def wrapper(*arg, **kwargs):
+            nonlocal count
+            count += 1
+            if n == 1:
+                return func(*arg, **kwargs)
+            if n == -1:
+                return None
+            if n > 0:
+                if count == 1:
+                    return func(*arg, **kwargs)
+                if (count - 1) % n == 0:
+                    return func(*arg, **kwargs)
+                return None
+            if n < 0:
+                abs_n = abs(n)
+                if count % abs_n == 0:
+                    return None
+                return func(*arg, **kwargs)
+        return wrapper
+    return decorator
