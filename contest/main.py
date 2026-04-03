e4287fd88998132f8d7af784f2c5234f441fb3d5
valya <valia2006@DESKTOP-KJRGD7H.localdomain>
2026-04-03 16:54:14 +0300

    3

--- contest/main.py
+++ contest/main.py
@@ -4,11 +4,27 @@ def array_diff(a: list, b: list) -> list:
 
 def sum_pairs(nums: list, sum: int) -> list:
     """Решение для Задачи 2"""
-    pass
+    result = set()
+    for num in nums:
+        a = sum - num
+        if a in result:
+            return [a, num]
+        result.add(num)
+    return None
+
+
 
 def remove_duplicate_ids(obj: dict) -> dict:
     """Решение для Задачи 3"""
-    pass
+    used_chars = set()
+    result = {}
+    keys_sort = sorted(obj.keys(), key=int, reverse=True)
+    for key in keys_sort:
+        unique = list(dict.fromkeys(obj[key]))
+        filtered = list(filter(lambda x: x not in used_chars, unique))
+        used_chars.update(filtered)
+        result[key] = filtered
+    return result
 
 # В данной функции определите самостоятельно, что она принимает, а что возвращает
 def lazy():
